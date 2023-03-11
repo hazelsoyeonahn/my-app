1ca78f6d9a6ecd61828487186c7bd6839432969c
Hazel Ahn <soyeoni@hotmail.co.kr>
2023-03-11 21:18:36 +0900

    Netflix Clone App practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+
+function Hello(){
+  function destroyFn(){
+    console.log("destroyed");
+  }
+  function effectFn(){
+    console.log("created");
+    return destroyFn; //return cleanup function if destoryed
+  }
+  useEffect(effectFn,[]);
+  return <h1>Hello</h1>;
+}
+//    return () => console.log("destroyed"); //clean up function: do something if component is destroyed
+
 
 function App() {
+  const[showing, setShowing] = useState(false);
+  const onClick = () =>setShowing((prev) => !prev);
+    /*
+  //state
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  //function
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {setKeyword(event.target.value)};
+  
+  console.log("I run all the time");
+  const onlyOnce = () => {
+    console.log("I run only once");
+  }
+
+  //useEffect allows the code to run only once
+  useEffect(onlyOnce, []);
+
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
+
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 4){
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]); //array is used when you would like the changes specifically*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   /* <div>
+      <input value = {keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me! </button>
+      </div>*/
+
+      <div>
+        {showing ? <Hello /> : null}
+        <button onClick={onClick}>{showing? "Hiding" : "Show"}</button>
+      </div>
   );
 }
 
